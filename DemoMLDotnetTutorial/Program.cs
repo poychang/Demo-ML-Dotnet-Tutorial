4b75dbf61a2b889cbd41be6bf82141ad89e7c4a8
Poy Chang <poypost@gmail.com>
2018-10-15 23:10:05 +0800

    Add project files.

---
+++ DemoMLDotnetTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoMLDotnetTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
